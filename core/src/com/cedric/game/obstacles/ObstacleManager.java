23222d4a15ee0b6881d864d8be5ec6229a5a0fc8
MartensCedric <cedricmartens98@gmail.com>
2016-09-09 22:14:34 -0400

    Added rocks, not functional yet, bugs to fix

--- core/src/com/cedric/game/obstacles/ObstacleManager.java
+++ core/src/com/cedric/game/obstacles/ObstacleManager.java
@@ -13,7 +13,9 @@ import java.util.Random;
  */
 public class ObstacleManager implements Renderable{
 
-    private ArrayList<Spike> spikesFloor;
+    private ArrayList<Rock> rocks;
+    private int rockSpawningTempo;
+    private boolean rocksAreSpawning;
 
     private ClashingGravity game;
 
@@ -22,6 +24,8 @@ public class ObstacleManager implements Renderable{
     public ObstacleManager(ClashingGravity game)
     {
         this.game = game;
+        rockSpawningTempo = 2000;
+        rocksAreSpawning = true;
 
         walls = new WallManager(game);
     }
@@ -29,11 +33,52 @@ public class ObstacleManager implements Renderable{
     public void update()
     {
         walls.update();
+
+        //TODO FIX THIS SHIT, You made a  fucking while in update
+        //Put a bool (rocksarespawning) to keep it at 2s interval
+        Thread rockSpawningClock = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(rocksAreSpawning)
+                    {
+                        Thread.sleep(rockSpawningTempo);
+
+                        if(rockSpawningTempo > 500)
+                            rockSpawningTempo -= 10;
+
+                        spawnRock();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        for(Rock rock : rocks)
+            rock.move();
     }
 
     @Override
     public void render() {
 
         walls.render();
+
+        for(Rock rock : rocks)
+        {
+            rock.render();
+        }
+    }
+
+    private void spawnRock()
+    {
+        //We want to spawn walls that won't touch spikes
+        Random randomizer = new Random();
+
+        //Wall size is 16 and so is spike
+        //So we need to exclude 64 pixels
+
+        int rockY = randomizer.nextInt(Gdx.graphics.getHeight() - 32) + 32;
+        rocks.add(new Rock(Gdx.graphics.getWidth(), rockY, game));
     }
 }
--- core/src/com/cedric/game/obstacles/Rock.java
+++ core/src/com/cedric/game/obstacles/Rock.java
@@ -1,7 +1,52 @@
 package com.cedric.game.obstacles;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.cedric.game.ClashingGravity;
+import com.cedric.game.interfaces.KillsPlayerOnTouch;
+import com.cedric.game.interfaces.Renderable;
+
 /**
  * Created by Cedric on 2016-09-09.
  */
-public class Rock {
+public class Rock implements Renderable, KillsPlayerOnTouch{
+
+    private Texture texture;
+    private ClashingGravity game;
+
+    private int x;
+    private int y;
+
+    private int width;
+    private int height;
+
+    public Rock(int x, int y, ClashingGravity game) {
+
+        this.game = game;
+        this.texture = game.getSprite("obs_rock");
+        this.width = texture.getWidth();
+        this.height = texture.getHeight();
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move()
+    {
+        this.x -= game.getPlayerSpeed();
+        checkForCollisionWithPlayer();
+    }
+
+    @Override
+    public void checkForCollisionWithPlayer(){
+        Rectangle rock = new Rectangle(this.x, this.y, this.width, this.height);
+        Rectangle player = new Rectangle((float)game.getPlayer().getX(), (float)game.getPlayer().getY(), 32, 32);
+
+        if(rock.overlaps(player))
+            game.getPlayer().deathAnimation();
+    }
+
+    @Override
+    public void render() {
+        game.getBatch().draw(texture, x, y);
+    }
 }
